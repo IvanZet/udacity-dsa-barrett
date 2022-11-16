b555c8c75ab529f80c3cf2a622ac043607434151
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-11-16 17:29:59 +0100

    Adds constructor and hash() of HashTable

---
+++ src/main/java/net/ivanzykov/datastructures/hashtable/HashTable.java
@@ -0,0 +1,47 @@
+package net.ivanzykov.datastructures.hashtable;
+
+public class HashTable {
+
+    private int size = 7;
+    private Node[] dataMap;
+
+    class Node {
+
+        private String key;
+        private int value;
+        private Node next;
+
+        public Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public void printTable() {
+        for(int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            if(dataMap[i] != null) {
+                Node temp = dataMap[i];
+                while (temp != null) {
+                    System.out.println("   {" + temp.key + ", " + temp.value + "}");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+
+    // FIXME: change access to private. It is public now to test it
+    public int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
+    }
+}
