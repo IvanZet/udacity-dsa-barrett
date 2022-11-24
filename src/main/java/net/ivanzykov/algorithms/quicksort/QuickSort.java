c19fb6db25eb997863c61c410c616d13c0b62c0b
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-11-24 15:53:26 +0100

    Finishes Quick sort

--- src/main/java/net/ivanzykov/algorithms/quicksort/QuickSort.java
+++ src/main/java/net/ivanzykov/algorithms/quicksort/QuickSort.java
@@ -4,6 +4,18 @@ import java.util.Arrays;
 
 public class QuickSort {
 
+    public static void quickSortHelper(int[] array, int left, int right) {
+        if (left < right) {
+            int pivotIndex = pivot(array, left, right);
+            quickSortHelper(array, left, pivotIndex - 1);
+            quickSortHelper(array, pivotIndex + 1, right);
+        }
+    }
+
+    public static void quickSort(int[] array) {
+        quickSortHelper(array, 0, array.length - 1);
+    }
+
     private static void swap(int[] array, int firstIndex, int secondIndex) {
         int temp = array[firstIndex];
         array[firstIndex] = array[secondIndex];
@@ -26,17 +38,14 @@ public class QuickSort {
 
         int[] myArray = {4,6,1,7,3,2,5};
 
-        int returnedIndex = pivot(myArray, 0, 6);
-
-        System.out.println( "\nReturned Index: " + returnedIndex);
+        quickSort(myArray);
 
         System.out.println( Arrays.toString( myArray ) );
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            Returned Index: 3
-            [2, 1, 3, 4, 6, 7, 5]
+            [1, 2, 3, 4, 5, 6, 7]
 
          */
 
